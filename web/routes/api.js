0303b17fd60f32545fe57587733d9b5a94ce375b
leojpod <leo.jpod@gmail.com>
2016-10-04 14:57:35 +0200

    create a router for the API

---
+++ web/routes/api.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var router = express.Router()
+
+/**
+ * Brief description of what's required:
+  - one POST where we'll upload the PDF to analyse and allow for some customization
+ */
+
+module.exports = router
