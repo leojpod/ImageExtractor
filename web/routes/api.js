624bd44ba7d1f01d6351840791d6ea155540dd5f
leojpod <leo.jpod@gmail.com>
2016-10-27 12:59:30 +0200

    create 2 routes to deal with the file download and the cleanup once the process is completed

--- web/routes/api.js
+++ web/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express')
 var path = require('path')
 var multer = require('multer')
 var upload = multer({ dest: 'pdfs/' })
-var fs = require('fs')
+var fse = require('fs-extra')
 var spawn = require('child_process').spawn
 var router = express.Router()
 
@@ -12,7 +12,7 @@ var router = express.Router()
  */
 router.post('/extract', upload.single('pdfFile'), function (req, res) {
   let pdfFilePath = req.file.path
-  let outputPath = path.join(req.app.locals.publicFolder, `${req.file.filename}-imgs/`)
+  let outputPath = path.join(req.app.locals.publicFolder, `/pdf-images/${req.file.filename}/`)
   let child = spawn(
     'java',
     [
@@ -32,10 +32,10 @@ router.post('/extract', upload.single('pdfFile'), function (req, res) {
   })
   child.on('close', (code) => {
     console.log(`close code => ${code}`)
+    fse.unlink(pdfFilePath)
     if (code === 0) {
       // it worked, fetch the imgs and send the list of URLS to the front-end
-      fs.readdir(outputPath, (err, files) => {
-        fs.unlink(pdfFilePath)
+      fse.readdir(outputPath, (err, files) => {
         if (err) {
           res.status(500).json({error: `could not read the output folder`, errMessage: err})
           return
@@ -43,7 +43,7 @@ router.post('/extract', upload.single('pdfFile'), function (req, res) {
         res.status(200).json({
           id: `${req.file.filename}`,
           files: files.map((file) => {
-            return path.join(outputPath, file)
+            return `/pdf-images/${req.file.filename}/${file}`
           })
         })
       })
@@ -54,13 +54,31 @@ router.post('/extract', upload.single('pdfFile'), function (req, res) {
 })
 
 router.post('/clean/:id', function (req, res) {
-  let outputPath = `${req.params.id}-imgs`
-  fs.rmdir(outputPath, (err) => {
+  let outputPath = path.join(req.app.locals.publicFolder, `/pdf-images/${req.params.id}`)
+
+  fse.remove(outputPath, (err) => {
     if (err) {
       res.status(500).json({error: `an error occured`, err})
       return
     }
     res.status(200).json({status: 'ok'})
+  })
+})
+
+router.get('/download/', function (req, res) {
+  let picFile = req.query.pic
+  if (!picFile) {
+    res.status(400).json({error: 'missing required parameter: picFile'})
+    return
+  }
+  picFile = path.join(req.app.locals.publicFolder, picFile)
+  console.log('trying to download a picture: ', picFile)
+  fse.access(picFile, fse.constants.R_OK, (err) => {
+    if (err) {
+      res.status(404).json({error: 'image not existing'})
+    } else {
+      res.download(picFile)
+    }
   })
 })
 
